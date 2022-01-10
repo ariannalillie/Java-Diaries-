6993b615d9aa9cd6272a284d37ead75863c4bf8d
Arianna Johnson <ariannajohnson588@gmail.com>
2022-01-10 12:48:41 -0500

    Java basics

---
+++ notes/Java.md
@@ -0,0 +1,11 @@
+# Java
+Sun Microsystems released the Java programming language in 1995. Java is known for being simple, portable, secure and robust.
+
+## Java Classes
+Classes can be though of as a single concept. Each file has one primary class named after the file. 
+
+```Java
+public class HelloWorld {
+
+}
+```
---
+++ print/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main
+    (String[] args) {
+        System.out.println("Hello World!")
+    }
+
+}
+
+// System - is a build in Java class that contains useful tools for other programs
+// out - is short for 'output'
+// println - is short for print line (outputs everything on the same line)
+
